19a31bceed1426a339f176ced99ebfabee3c84bc
Wei-zh777 <2732268581@qq.com>
2024-12-01 01:03:06 +0800

    开始界面
    开始，退出，还有按键反馈及音效

---
+++ UI/start_screen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var click_sound:AudioStreamPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func hook_button_sound():
+	click_sound.play()
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://场景/world.tscn")
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
